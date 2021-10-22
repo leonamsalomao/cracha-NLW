d521d26e3188bb7d3aac351831c653d73c0b7c84
leonamsalomao <70729829+leonamsalomao@users.noreply.github.com>
2021-10-22 17:34:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+/* function mostreMe(texto){
+        alert(texto)
+      }
+      const links = {
+        github: 'jakeliny',
+        youtube: 'jakelinygracielly',
+        facebook: 'maykbrito',
+        instagram: 'jakeliny',
+        twitter: 'jakelinytec'
+      }
+
+      mostreMe(links.github) */
+
+const linksSocialMedia = {
+  github: 'jakeliny',
+  youtube: 'jakelinygracielly',
+  facebook: 'maykbrito',
+  instagram: 'jakeliny',
+  twitter: 'jakelinytec'
+}
+
+function changeSocialMediaLinks() {
+  /* document.getElementById('userName').textContent = 'Léo Salomão' */
+
+  /*  userName.textContent = 'Sheldon Lee Cooper' */
+  /* for(let i= 0; i<= 10; i++) */
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      useLink.href = data.html_url
+      userImage.src = data.avatar_ulr
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
+
+/* Arrow functions */
+
+/* function nomeDaFuncao(argumentos){
+  //code
+
+}
+
+argumentos => {
+  
+}
+
+nomeDaFuncao() */
